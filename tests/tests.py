7cb7b01e26f46441bf6bdf99b68dc53ec7640331
Gregory Ashton <gregory.ashton@ligo.org>
2016-09-21 11:23:59 +0200

    Remove old methods
    Removes the fully-coherent search which was just a wrapper to CFS_v2 and
    all of the `slow' methods which used command line arguments rather than
    the python interface.

--- tests/tests.py
+++ tests/tests.py
@@ -75,41 +75,6 @@ class TestBaseSearchClass(unittest.TestCase):
                 rtol=1e-9, atol=1e-9))
 
 
-class TestFullyCoherentNarrowBandSearch(unittest.TestCase):
-    label = "Test"
-    outdir = 'TestData'
-
-    def test_compute_fstat(self):
-        Writer = glitch_tools.Writer(self.label, outdir=self.outdir)
-        Writer.make_data()
-
-        search = glitch_searches.FullyCoherentNarrowBandSearch(
-            self.label, self.outdir, tref=Writer.tref, Alpha=Writer.Alpha,
-            Delta=Writer.Delta, duration=Writer.duration, tstart=Writer.tstart,
-            Writer=Writer)
-        search.run_computefstatistic_slow(m=1e-3, n=0)
-        _, _, _, FS_max_slow = search.get_FS_max()
-
-        search.run_computefstatistic(dFreq=0, numFreqBins=1)
-        _, _, _, FS_max = search.get_FS_max()
-        self.assertTrue(
-            np.abs(FS_max-FS_max_slow)/FS_max_slow < 0.1)
-
-    def test_compute_fstat_against_predict_fstat(self):
-        Writer = glitch_tools.Writer(self.label, outdir=self.outdir)
-        Writer.make_data()
-        Writer.run_makefakedata()
-        predicted_FS = Writer.predict_fstat()
-
-        search = glitch_searches.FullyCoherentNarrowBandSearch(
-            self.label, self.outdir, tref=Writer.tref, Alpha=Writer.Alpha,
-            Delta=Writer.Delta, duration=Writer.duration, tstart=Writer.tstart,
-            Writer=Writer)
-        search.run_computefstatistic(dFreq=0, numFreqBins=1)
-        _, _, _, FS_max = search.get_FS_max()
-        self.assertTrue(np.abs(predicted_FS-FS_max)/FS_max < 0.5)
-
-
 class TestSemiCoherentGlitchSearch(unittest.TestCase):
     label = "Test"
     outdir = 'TestData'
@@ -132,58 +97,6 @@ class TestSemiCoherentGlitchSearch(unittest.TestCase):
                                                        Writer.Delta)
         print predicted_FS, FS
         self.assertTrue(np.abs(predicted_FS-FS)/FS < 0.1)
-
-    def test_run_computefstatistic_single_point_slow(self):
-        Writer = pyfstat.Writer(self.label, outdir=self.outdir)
-        Writer.make_data()
-        predicted_FS = Writer.predict_fstat()
-
-        search = pyfstat.SemiCoherentGlitchSearch(
-            label=Writer.label, outdir=Writer.outdir, tref=Writer.tref,
-            tstart=Writer.tstart, tend=Writer.tend)
-        FS = search.run_computefstatistic_single_point_slow(search.tref,
-                                                            search.tstart,
-                                                            search.tend,
-                                                            Writer.F0,
-                                                            Writer.F1,
-                                                            Writer.F2,
-                                                            Writer.Alpha,
-                                                            Writer.Delta)
-        self.assertTrue(np.abs(predicted_FS-FS)/FS < 0.1)
-
-    def test_compute_glitch_fstat_slow(self):
-        duration = 100*86400
-        dtglitch = 100*43200
-        delta_F0 = 0
-        Writer = pyfstat.Writer(self.label, outdir=self.outdir,
-                                duration=duration, dtglitch=dtglitch,
-                                delta_F0=delta_F0)
-        Writer.make_data()
-
-        search = pyfstat.SemiCoherentGlitchSearch(
-            label=Writer.label, outdir=Writer.outdir, tref=Writer.tref,
-            tstart=Writer.tstart, tend=Writer.tend)
-
-        FS = search.compute_glitch_fstat_slow(Writer.F0, Writer.F1, Writer.F2,
-                                              Writer.Alpha, Writer.Delta,
-                                              Writer.delta_F0, Writer.delta_F1,
-                                              Writer.tglitch)
-
-        # Compute the predicted semi-coherent glitch Fstat
-        tstart = Writer.tstart
-        tend = Writer.tend
-
-        Writer.tend = tstart + dtglitch
-        FSA = Writer.predict_fstat()
-
-        Writer.tstart = tstart + dtglitch
-        Writer.tend = tend
-        FSB = Writer.predict_fstat()
-
-        predicted_FS = .5*(FSA + FSB)
-
-        print(predicted_FS, FS)
-        self.assertTrue(np.abs((FS - predicted_FS))/predicted_FS < 0.1)
 
     def test_compute_nglitch_fstat(self):
         duration = 100*86400
