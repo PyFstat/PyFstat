36911fa4d25c9cfb8664493af891444db8a72d49
gregory.ashton <gregory.ashton@ligo.org>
2016-12-20 11:41:19 +0100

    Adds macros for NoiseOnly study and forgotten files

--- Paper/DirectedMCNoiseOnly/plot_data.py
+++ Paper/DirectedMCNoiseOnly/plot_data.py
@@ -33,12 +33,12 @@ print 'Number of samples = ', len(df)
 fig, ax = plt.subplots()
 ax.hist(df.twoF, bins=50, histtype='step', color='k', normed=True, linewidth=1)
 twoFsmooth = np.linspace(0, df.twoF.max(), 100)
-# ax.plot(twoFsmooth, maxtwoFinNoise(twoFsmooth, 8e5), '-r')
+# ax.plot(twoFsmooth, maxtwoFinNoise(twoFsmooth, 2e3), '-r')
 ax.set_xlabel('$\widetilde{2\mathcal{F}}$')
 ax.set_xlim(0, 60)
 fig.tight_layout()
-fig.savefig('allsky_noise_twoF_histogram.png')
+fig.savefig('directed_noise_twoF_histogram.png')
 
 from latex_macro_generator import write_to_macro
-write_to_macro('AllSkyMCNoiseOnlyMaximum', '{:1.1f}'.format(np.max(df.twoF)),
+write_to_macro('DirectedMCNoiseOnlyMaximum', '{:1.1f}'.format(np.max(df.twoF)),
                '../macros.tex')
