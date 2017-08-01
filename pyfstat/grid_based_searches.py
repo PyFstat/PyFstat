efd81d4b3726b270c55ccd26f0edbcd8d8ec458d
Gregory Ashton <gregory.ashton@ligo.org>
2017-08-01 10:23:41 +0200

    Adds SSBprec to uniform prior search

--- pyfstat/grid_based_searches.py
+++ pyfstat/grid_based_searches.py
@@ -279,9 +279,11 @@ class GridSearch(BaseSearchClass):
 
 
 class GridUniformPriorSearch():
+    @helper_functions.initializer
     def __init__(self, theta_prior, NF0, NF1, label, outdir, sftfilepattern,
                  tref, minStartTime, maxStartTime, minCoverFreq=None,
-                 maxCoverFreq=None, BSGL=False, detectors=None, nsegs=1):
+                 maxCoverFreq=None, BSGL=False, detectors=None, nsegs=1,
+                 SSBprec=None):
         dF0 = (theta_prior['F0']['upper'] - theta_prior['F0']['lower'])/NF0
         dF1 = (theta_prior['F1']['upper'] - theta_prior['F1']['lower'])/NF1
         F0s = [theta_prior['F0']['lower'], theta_prior['F0']['upper'], dF0]
@@ -291,7 +293,7 @@ class GridUniformPriorSearch():
             Alphas=[theta_prior['Alpha']], Deltas=[theta_prior['Delta']],
             minStartTime=minStartTime, maxStartTime=maxStartTime, BSGL=BSGL,
             detectors=detectors, minCoverFreq=minCoverFreq,
-            maxCoverFreq=maxCoverFreq, nsegs=nsegs)
+            maxCoverFreq=maxCoverFreq, nsegs=nsegs, SSBprec=SSBprec)
 
     def run(self):
         self.search.run()
