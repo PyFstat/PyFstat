73fee7871018e72ed6ba965d8cae343ebc89f8a8
David Keitel <david.keitel@ligo.org>
2017-08-15 17:31:29 +0100

    configurable transient windows:
     -replaces boolean 'transient' flag
     -window type [none,rect,exp]
      for both injections and searches
     -t0, tau bands for gridsearch,
      single-point use (e.g. MCMC) should be unchanged

--- pyfstat/grid_based_searches.py
+++ pyfstat/grid_based_searches.py
@@ -31,7 +31,8 @@ class GridSearch(BaseSearchClass):
                  Deltas, tref=None, minStartTime=None, maxStartTime=None,
                  nsegs=1, BSGL=False, minCoverFreq=None, maxCoverFreq=None,
                  detectors=None, SSBprec=None, injectSources=None,
-                 input_arrays=False, assumeSqrtSX=None):
+                 input_arrays=False, assumeSqrtSX=None,
+                 transientWindowType=None, t0Band=None, tauBand=None):
         """
         Parameters
         ----------
@@ -48,6 +49,17 @@ class GridSearch(BaseSearchClass):
             GPS seconds of the reference time, start time and end time
         input_arrays: bool
             if true, use the F0s, F1s, etc as is
+        transientWindowType: str
+            If 'rect' or 'exp',
+            compute atoms so that a transient (t0,tau) map can later be computed.
+            ('none' instead of None explicitly calls the transient-window function,
+             but with the full range, for debugging)
+            Currently only supported for nsegs=1.
+        t0Band, tauBand: int
+            if >0, search t0 in (minStartTime,minStartTime+t0Band)
+                   and tau in (2*Tsft,2*Tsft+tauBand).
+            if =0, only compute CW Fstat with t0=minStartTime,
+                   tau=maxStartTime-minStartTime.
 
         For all other parameters, see `pyfstat.ComputeFStat` for details
         """
@@ -66,7 +78,9 @@ class GridSearch(BaseSearchClass):
             self.search = ComputeFstat(
                 tref=self.tref, sftfilepattern=self.sftfilepattern,
                 minCoverFreq=self.minCoverFreq, maxCoverFreq=self.maxCoverFreq,
-                detectors=self.detectors, transient=False,
+                detectors=self.detectors,
+                transientWindowType=self.transientWindowType,
+                t0Band=self.t0Band, tauBand=self.tauBand,
                 minStartTime=self.minStartTime, maxStartTime=self.maxStartTime,
                 BSGL=self.BSGL, SSBprec=self.SSBprec,
                 injectSources=self.injectSources,
@@ -563,7 +577,7 @@ class FrequencySlidingWindow(GridSearch):
         self.search = ComputeFstat(
             tref=self.tref, sftfilepattern=self.sftfilepattern,
             minCoverFreq=self.minCoverFreq, maxCoverFreq=self.maxCoverFreq,
-            detectors=self.detectors, transient=True,
+            detectors=self.detectors, transientWindowType=self.transientWindowType,
             minStartTime=self.minStartTime, maxStartTime=self.maxStartTime,
             BSGL=self.BSGL, SSBprec=self.SSBprec,
             injectSources=self.injectSources)
--- pyfstat/make_sfts.py
+++ pyfstat/make_sfts.py
@@ -25,7 +25,8 @@ class Writer(BaseSearchClass):
                  tref=None, F0=30, F1=1e-10, F2=0, Alpha=5e-3,
                  Delta=6e-2, h0=0.1, cosi=0.0, psi=0.0, phi=0, Tsft=1800,
                  outdir=".", sqrtSX=1, Band=4, detectors='H1',
-                 minStartTime=None, maxStartTime=None, add_noise=True):
+                 minStartTime=None, maxStartTime=None, add_noise=True,
+                 transientWindowType='none'):
         """
         Parameters
         ----------
@@ -92,7 +93,7 @@ class Writer(BaseSearchClass):
         self.run_makefakedata()
 
     def get_single_config_line(self, i, Alpha, Delta, h0, cosi, psi, phi, F0,
-                               F1, F2, tref, tstart, duration_days):
+                               F1, F2, tref, window, tstart, duration_days):
         template = (
 """[TS{}]
 Alpha = {:1.18e}
@@ -105,11 +106,11 @@ Freq = {:1.18e}
 f1dot = {:1.18e}
 f2dot = {:1.18e}
 refTime = {:10.6f}
-transientWindowType=rect
-transientStartTime={:10.3f}
-transientTauDays={:1.3f}\n""")
+transientWindowType = {:s}
+transientStartTime = {:10.3f}
+transientTauDays = {:1.3f}\n""")
         return template.format(i, Alpha, Delta, h0, cosi, psi, phi, F0, F1,
-                               F2, tref, tstart, duration_days)
+                               F2, tref, window, tstart, duration_days)
 
     def make_cff(self):
         """
@@ -119,7 +120,7 @@ transientTauDays={:1.3f}\n""")
 
         content = self.get_single_config_line(
             0, self.Alpha, self.Delta, self.h0, self.cosi, self.psi,
-            self.phi, self.F0, self.F1, self.F2, self.tref, self.tstart,
+            self.phi, self.F0, self.F1, self.F2, self.tref, self.transientWindowType, self.tstart,
             self.duration_days)
 
         if self.check_if_cff_file_needs_rewritting(content):
@@ -247,7 +248,8 @@ class GlitchWriter(Writer):
                  delta_F2=0, tref=None, F0=30, F1=1e-10, F2=0, Alpha=5e-3,
                  Delta=6e-2, h0=0.1, cosi=0.0, psi=0.0, phi=0, Tsft=1800,
                  outdir=".", sqrtSX=1, Band=4, detectors='H1',
-                 minStartTime=None, maxStartTime=None, add_noise=True):
+                 minStartTime=None, maxStartTime=None, add_noise=True,
+                 transientWindowType='rect'):
         """
         Parameters
         ----------
@@ -317,7 +319,7 @@ class GlitchWriter(Writer):
                                            self.tbounds[:-1])):
             line = self.get_single_config_line(
                 i, self.Alpha, self.Delta, self.h0, self.cosi, self.psi,
-                t[0], t[1], t[2], t[3], self.tref, ts, d)
+                t[0], t[1], t[2], t[3], self.tref, self.transientWindowType, ts, d)
 
             content += line
 
