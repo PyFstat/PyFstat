11f8d4be21a094124f81883a876c82c0e0ca2485
David Keitel <david.keitel@ligo.org>
2020-03-10 14:14:00 +0100

    improve semi_coherent_directed_follow_up example outputs

--- examples/followup_examples/semi_coherent_directed_follow_up.py
+++ examples/followup_examples/semi_coherent_directed_follow_up.py
@@ -76,7 +76,7 @@ mcmc = pyfstat.MCMCFollowUpSearch(
 NstarMax = 1000
 Nsegs0 = 100
 fig, axes = plt.subplots(nrows=2, figsize=(3.4, 3.5))
-fig, axes = mcmc.run(
+mcmc.run(
     NstarMax=NstarMax,
     Nsegs0=Nsegs0,
     labelpad=0.01,
@@ -92,3 +92,6 @@ for ax in axes:
 axes[-1].set_xlabel(r"$\textrm{Number of steps}$", labelpad=0.1)
 fig.tight_layout()
 fig.savefig(os.path.join(mcmc.outdir, mcmc.label + "_walkers.png"), dpi=400)
+
+mcmc.plot_corner(add_prior=True)
+mcmc.print_summary()
