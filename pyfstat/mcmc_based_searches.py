5e91d8623fc115607d9a45493c8c773ad9c14849
David Keitel <david.keitel@ligo.org>
2020-03-10 15:36:12 +0100

    Revert "also use r-strings in f.write calls"
    This reverts commit db37a5e446502bc1d1da395f9d256d233a4cc215.

--- pyfstat/mcmc_based_searches.py
+++ pyfstat/mcmc_based_searches.py
@@ -1700,16 +1700,16 @@ class MCMCSearch(core.BaseSearchClass):
 
         logging.info("Writing par file with max twoF = {}".format(max_twoF))
         with open(filename, "w+") as f:
-            f.write(r"MaxtwoF = {}\n".format(max_twoF))
-            f.write(r"tref = {}\n".format(self.tref))
+            f.write("MaxtwoF = {}\n".format(max_twoF))
+            f.write("tref = {}\n".format(self.tref))
             if hasattr(self, "theta0_index"):
-                f.write(r"theta0_index = {}\n".format(self.theta0_idx))
+                f.write("theta0_index = {}\n".format(self.theta0_idx))
             if method == "med":
                 for key, val in median_std_d.items():
-                    f.write(r"{} = {:1.16e}\n".format(key, val))
+                    f.write("{} = {:1.16e}\n".format(key, val))
             if method == "twoFmax":
                 for key, val in max_twoF_d.items():
-                    f.write(r"{} = {:1.16e}\n".format(key, val))
+                    f.write("{} = {:1.16e}\n".format(key, val))
 
     def generate_loudest(self):
         """ Use lalapps_ComputeFstatistic_v2 to produce a .loudest file """
@@ -1766,11 +1766,11 @@ class MCMCSearch(core.BaseSearchClass):
 
                     u = self.unit_dictionary[key]
                     s = self.symbol_dictionary[key]
-                    f.write(r"\n")
+                    f.write("\n")
                     a = helper_functions.texify_float(a)
                     b = helper_functions.texify_float(b)
-                    f.write(r" " + line.format(s, a, b, u) + r" \\")
-            f.write(r"\n\end{tabular}\n")
+                    f.write(" " + line.format(s, a, b, u) + r" \\")
+            f.write("\n\end{tabular}\n")
 
     def print_summary(self):
         """ Prints a summary of the max twoF found to the terminal """
@@ -1926,7 +1926,7 @@ class MCMCSearch(core.BaseSearchClass):
     def write_evidence_file_from_dict(self, EvidenceDict, evidence_file_name):
         with open(evidence_file_name, "w+") as f:
             for key, val in EvidenceDict.items():
-                f.write(r"{} {} {}\n".format(key, val[0], val[1]))
+                f.write("{} {} {}\n".format(key, val[0], val[1]))
 
 
 class MCMCGlitchSearch(MCMCSearch):
@@ -2773,7 +2773,7 @@ class MCMCFollowUpSearch(MCMCSemiCoherentSearch):
                     r"Stage & $N_\mathrm{seg}$ &"
                     r"$T_\mathrm{coh}^{\rm days}$ &"
                     r"$\mathcal{N}^*(\Nseg^{(\ell)}, \Delta\mathbf{\lambda}^{(0)})$ \\ \hline"
-                    r"\n"
+                    "\n"
                 )
                 for i, rs in enumerate(run_setup):
                     Tcoh = float(self.maxStartTime - self.minStartTime) / rs[1] / 86400
