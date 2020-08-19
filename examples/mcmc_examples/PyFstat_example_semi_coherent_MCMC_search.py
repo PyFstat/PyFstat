178154930b672c6cbf7894495091af38e7f730c7
Rodrigo-Tenorio <rodrigo.tenorio.marquez@gmail.com>
2020-08-19 18:06:36 +0200

    mcmc_examples/Semicoherent search

--- examples/mcmc_examples/PyFstat_example_semi_coherent_MCMC_search.py
+++ examples/mcmc_examples/PyFstat_example_semi_coherent_MCMC_search.py
@@ -6,35 +6,30 @@ label = os.path.splitext(os.path.basename(__file__))[0]
 outdir = os.path.join("PyFstat_example_data", label)
 
 # Properties of the GW data
-sqrtSX = 1e-23
-tstart = 1000000000
-duration = 100 * 86400
-tend = tstart + duration
+data_parameters = {
+    "sqrtSX": 1e-23,
+    "tstart": 1000000000,
+    "duration": 100 * 86400,
+    "detectors": "H1",
+}
+tend = data_parameters["tstart"] + data_parameters["duration"]
+mid_time = 0.5 * (data_parameters["tstart"] + tend)
 
 # Properties of the signal
-F0 = 30.0
-F1 = -1e-10
-F2 = 0
-Alpha = np.radians(83.6292)
-Delta = np.radians(22.0144)
-tref = 0.5 * (tstart + tend)
-
 depth = 10
-h0 = sqrtSX / depth
+signal_parameters = {
+    "F0": 30.0,
+    "F1": -1e-10,
+    "F2": 0,
+    "Alpha": np.radians(83.6292),
+    "Delta": np.radians(22.0144),
+    "tref": mid_time,
+    "h0": data_parameters["sqrtSX"] / depth,
+    "cosi": 1.0,
+}
 
 data = pyfstat.Writer(
-    label=label,
-    outdir=outdir,
-    tref=tref,
-    tstart=tstart,
-    F0=F0,
-    F1=F1,
-    F2=F2,
-    duration=duration,
-    Alpha=Alpha,
-    Delta=Delta,
-    h0=h0,
-    sqrtSX=sqrtSX,
+    label=label, outdir=outdir, **data_parameters, **signal_parameters
 )
 data.make_data()
 
@@ -44,17 +39,24 @@ print("Predicted twoF value: {}\n".format(twoF))
 
 DeltaF0 = 1e-7
 DeltaF1 = 1e-13
-VF0 = (np.pi * duration * DeltaF0) ** 2 / 3.0
-VF1 = (np.pi * duration ** 2 * DeltaF1) ** 2 * 4 / 45.0
+VF0 = (np.pi * data_parameters["duration"] * DeltaF0) ** 2 / 3.0
+VF1 = (np.pi * data_parameters["duration"] ** 2 * DeltaF1) ** 2 * 4 / 45.0
 print("\nV={:1.2e}, VF0={:1.2e}, VF1={:1.2e}\n".format(VF0 * VF1, VF0, VF1))
 
 theta_prior = {
-    "F0": {"type": "unif", "lower": F0 - DeltaF0 / 2.0, "upper": F0 + DeltaF0 / 2.0},
-    "F1": {"type": "unif", "lower": F1 - DeltaF1 / 2.0, "upper": F1 + DeltaF1 / 2.0},
-    "F2": F2,
-    "Alpha": Alpha,
-    "Delta": Delta,
+    "F0": {
+        "type": "unif",
+        "lower": signal_parameters["F0"] - DeltaF0 / 2.0,
+        "upper": signal_parameters["F0"] + DeltaF0 / 2.0,
+    },
+    "F1": {
+        "type": "unif",
+        "lower": signal_parameters["F1"] - DeltaF1 / 2.0,
+        "upper": signal_parameters["F1"] + DeltaF1 / 2.0,
+    },
 }
+for key in "F2", "Alpha", "Delta":
+    theta_prior[key] = signal_parameters[key]
 
 ntemps = 1
 log10beta_min = -1
@@ -67,8 +69,8 @@ mcmc = pyfstat.MCMCSemiCoherentSearch(
     nsegs=10,
     sftfilepattern=os.path.join(outdir, "*{}*sft".format(label)),
     theta_prior=theta_prior,
-    tref=tref,
-    minStartTime=tstart,
+    tref=mid_time,
+    minStartTime=data_parameters["tstart"],
     maxStartTime=tend,
     nsteps=nsteps,
     nwalkers=nwalkers,
@@ -76,8 +78,8 @@ mcmc = pyfstat.MCMCSemiCoherentSearch(
     log10beta_min=log10beta_min,
 )
 mcmc.transform_dictionary = dict(
-    F0=dict(subtractor=F0, symbol="$f-f^\mathrm{s}$"),
-    F1=dict(subtractor=F1, symbol="$\dot{f}-\dot{f}^\mathrm{s}$"),
+    F0=dict(subtractor=signal_parameters["F0"], symbol="$f-f^\mathrm{s}$"),
+    F1=dict(subtractor=signal_parameters["F1"], symbol="$\dot{f}-\dot{f}^\mathrm{s}$"),
 )
 mcmc.run()
 mcmc.plot_corner(add_prior=True)
