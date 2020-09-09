PyFstat/PyFstat

commit 523454eecda3446cb2400efe72eb2460bde79552

Writer: no more default for detectors, drop internal reformatting
 -no Writer.IFOs attribute with extra quotation marks any more,
  just Writer.detectors kept and used unmodified

pyfstat/make_sfts.py
@@ -50,7 +50,7 @@ class Writer(BaseSearchClass):
         SFTWindowType=None,
         SFTWindowBeta=0.0,
         Band=None,
-        detectors="H1",
+        detectors=None,
         minStartTime=None,
         maxStartTime=None,
         transientWindowType="none",
@@ -144,7 +144,6 @@ class Writer(BaseSearchClass):
             )
             for ind, dets in enumerate(IFOs)
         ]
-        self.IFOs = ",".join(['"{}"'.format(d) for d in IFOs])
 
     def _get_setup_from_noiseSFTs(self):
         """
@@ -208,13 +207,14 @@ class Writer(BaseSearchClass):
             )
         self.tstart = min(tstart)
         self.duration = max(tend) - self.tstart
-        self.IFOs = ",".join(['"{}"'.format(d) for d in IFOs])
+        self.detectors = ",".join(IFOs)
 
     def basic_setup(self):
         os.makedirs(self.outdir, exist_ok=True)
         self.config_file_name = os.path.join(self.outdir, self.label + ".cff")
         self.theta = np.array([self.phi, self.F0, self.F1, self.F2])
 
+        no_noiseSFTs_options = ["tstart", "duration", "detectors"]
         if self.noiseSFTs is not None:
             logging.warning(
                 "noiseSFTs is not None: Inferring tstart, duration, Tsft. "
@@ -223,10 +223,14 @@ class Writer(BaseSearchClass):
                 "internal consistency accross input SFTs."
             )
             self._get_setup_from_noiseSFTs()
-        elif self.tstart is not None and self.duration is not None:
-            self._get_setup_from_tstart_duration()
+        elif np.any([getattr(self, k) is None for k in no_noiseSFTs_options]):
+            raise ValueError(
+                "Need either noiseSFTs or all of ({:s}).".format(
+                    ",".join(no_noiseSFTs_options)
+                )
+            )
         else:
-            raise ValueError("Need either noiseSFTs or both of (tstart,duration).")
+            self._get_setup_from_tstart_duration()
 
         self.sftfilepath = ";".join(
             [os.path.join(self.outdir, fn) for fn in self.sftfilenames]
@@ -559,7 +563,7 @@ transientTau = {:10.0f}\n"""
                 )
             cl_mfd.append('--noiseSFTs="{}"'.format(self.noiseSFTs))
         else:
-            cl_mfd.append("--IFOs={}".format(self.IFOs))
+            cl_mfd.append('--IFOs="{}"'.format(self.detectors))
         if self.sqrtSX:
             cl_mfd.append('--sqrtSX="{}"'.format(self.sqrtSX))
 
@@ -608,7 +612,7 @@ transientTau = {:10.0f}\n"""
             transientWindowType=self.transientWindowType,
             transientStartTime=self.transientStartTime,
             transientTau=self.transientTau,
-        )  # detectors OR IFO?
+        )
         return twoF_expected
 
 
@@ -643,7 +647,7 @@ class BinaryModulatedWriter(Writer):
         SFTWindowType=None,
         SFTWindowBeta=0.0,
         Band=None,
-        detectors="H1",
+        detectors=None,
         minStartTime=None,
         maxStartTime=None,
         transientWindowType="none",
@@ -813,7 +817,7 @@ class GlitchWriter(Writer):
         SFTWindowType=None,
         SFTWindowBeta=0.0,
         Band=None,
-        detectors="H1",
+        detectors=None,
         minStartTime=None,
         maxStartTime=None,
         transientWindowType="rect",
@@ -945,10 +949,9 @@ class FrequencyModulatedArtifactWriter(Writer):
         Pmod_amp=1,
         Alpha=None,
         Delta=None,
-        IFO="H1",
         minStartTime=None,
         maxStartTime=None,
-        detectors="H1",
+        detectors=None,
         randSeed=None,
     ):
         """
@@ -1025,10 +1028,14 @@ class FrequencyModulatedArtifactWriter(Writer):
             # Pos and vel returned in units of c
             DeltaFOrbital = np.dot(self.n, orbit_posvel.vel) * self.Fmax
 
-            if self.IFO == "H1":
+            if self.detectors == "H1":
                 Lambda = lal.LHO_4K_DETECTOR_LATITUDE_RAD
-            elif self.IFO == "L1":
+            elif self.detectors == "L1":
                 Lambda = lal.LLO_4K_DETECTOR_LATITUDE_RAD
+            else:
+                raise ValueError(
+                    "This class currently only supports detectors H1 or L1."
+                )
 
             DeltaFSpin = (
                 self.Pmod_amp
@@ -1052,8 +1059,8 @@ class FrequencyModulatedArtifactWriter(Writer):
 
     def concatenate_sft_files(self):
         SFTFilename = lalpulsar.OfficialSFTFilename(
-            self.IFO[0],
-            self.IFO[1],
+            self.detectors[0],
+            self.detectors[1],
             self.nsfts,
             self.Tsft,
             int(self.tstart),
@@ -1165,7 +1172,7 @@ class FrequencyModulatedArtifactWriter(Writer):
         cl_mfd.append("lalapps_Makefakedata_v4")
         cl_mfd.append("--outSingleSFT=FALSE")
         cl_mfd.append('--outSFTbname="{}"'.format(tmp_outdir))
-        cl_mfd.append("--IFO={}".format(self.IFO))
+        cl_mfd.append("--IFO={}".format(self.detectors))
         cl_mfd.append('--noiseSqrtSh="{}"'.format(self.sqrtSX))
         cl_mfd.append("--startTime={:0.0f}".format(mid_time - self.Tsft / 2.0))
         cl_mfd.append("--refTime={:0.0f}".format(mid_time))

commit 492b73951479c19fbe7b074816cb1f583cd68bda

Writer: fix check_cached_data_okay_to_use() for multiple IFOs
 -need to loop over self.sftfilepath.split(";")
 -also once again store MFD option --IFOs in quoted format matching
  its internal string-list handling (regression from 523454ee)
 -improve check logic and logging output
 -also fix method name
  check_if_cff_file_needs_rewritting() -> check_if_cff_file_needs_rewriting()
 -closes #161

pyfstat/make_sfts.py
@@ -394,45 +394,49 @@ class Writer(BaseSearchClass):
         else:
             logging.info("Writing config file {:s}...".format(self.config_file_name))
 
-        if self.check_if_cff_file_needs_rewritting(content):
+        if self.check_if_cff_file_needs_rewriting(content):
             config_file = open(self.config_file_name, "w+")
             config_file.write(content)
             config_file.close()
 
     def check_cached_data_okay_to_use(self, cl_mfd):
-        """ Check if cached data exists and, if it does, if it can be used """
+        """ Check if matching data already exists in SFT files and can be used. """
 
-        need_new = "Will create new SFT file."
+        need_new = "Will create new SFT file(s)."
 
-        logging.info("Checking if cached data good to reuse...")
-        if os.path.isfile(self.sftfilepath) is False:
-            logging.info(
-                "No SFT file matching {} found. {}".format(self.sftfilepath, need_new)
-            )
-            return False
-        else:
-            logging.info("OK: Matching SFT file found.")
+        logging.info("Checking if we can re-use existing SFT data file(s)...")
+        sftfiles = self.sftfilepath.split(";")
+        for sftfile in sftfiles:
+            if os.path.isfile(sftfile) is False:
+                logging.info(
+                    "...no SFT file matching '{}' found. {}".format(sftfile, need_new)
+                )
+                return False
+        logging.info("...OK: file(s) found matching '{}'.".format(sftfile))
 
         if "injectionSources" in cl_mfd:
             if os.path.isfile(self.config_file_name):
-                if os.path.getmtime(self.sftfilepath) < os.path.getmtime(
-                    self.config_file_name
+                if np.any(
+                    [
+                        os.path.getmtime(sftfile)
+                        < os.path.getmtime(self.config_file_name)
+                        for sftfile in sftfiles
+                    ]
                 ):
                     logging.info(
                         (
-                            "The config file {} has been modified since the SFT file {} "
-                            + "was created. {}"
+                            "...the config file '{}' has been modified since"
+                            " creation of the SFT file(s) '{}'. {}"
                         ).format(self.config_file_name, self.sftfilepath, need_new)
                     )
                     return False
                 else:
                     logging.info(
-                        "OK: The config file {} is older than the SFT file {}".format(
-                            self.config_file_name, self.sftfilepath
-                        )
+                        "...OK: The config file '{}' is older than the SFT file(s)"
+                        " '{}'.".format(self.config_file_name, self.sftfilepath)
                     )
                     # NOTE: at this point we assume it's safe to re-use, since
-                    # check_if_cff_file_needs_rewritting()
+                    # check_if_cff_file_needs_rewriting()
                     # should have already been called before
             else:
                 raise RuntimeError(
@@ -441,26 +445,35 @@ class Writer(BaseSearchClass):
                     )
                 )
 
-        logging.info("Checking new commandline against existing SFT header...")
-        catalog = lalpulsar.SFTdataFind(self.sftfilepath, None)
-        cl_old = helper_functions.get_lalapps_commandline_from_SFTDescriptor(
-            catalog.data[0]
-        )
-        if len(cl_old) == 0:
-            logging.info(
-                "Could not obtain comparison commandline from old SFT header. "
-                + need_new
+        logging.info("...checking new commandline against existing SFT header(s)...")
+        # here we check one SFT header from each SFT file,
+        # assuming that any concatenated file has been sanely constructed with
+        # matching CLs
+        for sftfile in sftfiles:
+            catalog = lalpulsar.SFTdataFind(sftfile, None)
+            cl_old = helper_functions.get_lalapps_commandline_from_SFTDescriptor(
+                catalog.data[0]
             )
-            return False
-        if not helper_functions.match_commandlines(cl_old, cl_mfd):
-            logging.info("Commandlines unmatched. " + need_new)
-            return False
-        else:
-            logging.info("OK: Commandline matched with old SFT header.")
-        logging.info("Looks like cached data matches current options, will re-use it!")
+            if len(cl_old) == 0:
+                logging.info(
+                    "......ould not obtain comparison commandline from first SFT"
+                    " header in old file '{}'. {}".format(sftfile, need_new)
+                )
+                return False
+            if not helper_functions.match_commandlines(cl_old, cl_mfd):
+                logging.info(
+                    "......commandlines unmatched for first SFT in old"
+                    " file '{}'. {}".format(sftfile, need_new)
+                )
+                return False
+        logging.info("......OK: Commandline matched with old SFT header(s).")
+        logging.info(
+            "...all data consistency checks passed: Looks like existing"
+            " SFT data matches current options, will re-use it!"
+        )
         return True
 
-    def check_if_cff_file_needs_rewritting(self, content):
+    def check_if_cff_file_needs_rewriting(self, content):
         """Check if the .cff file has changed
 
         Returns True if the file should be overwritten - where possible avoid
@@ -468,23 +481,25 @@ class Writer(BaseSearchClass):
         """
         logging.info("Checking if we can re-use injection config file...")
         if os.path.isfile(self.config_file_name) is False:
-            logging.info("No config file {} found.".format(self.config_file_name))
+            logging.info("...no config file {} found.".format(self.config_file_name))
             return True
         else:
-            logging.info("Config file {} already exists.".format(self.config_file_name))
+            logging.info(
+                "...OK: config file {} already exists.".format(self.config_file_name)
+            )
 
         with open(self.config_file_name, "r") as f:
             file_content = f.read()
             if file_content == content:
                 logging.info(
-                    "File contents match, no update of {} required.".format(
+                    "...OK: file contents match, no update of {} required.".format(
                         self.config_file_name
                     )
                 )
                 return False
             else:
                 logging.info(
-                    "File contents unmatched, updating {}.".format(
+                    "...file contents unmatched, updating {}.".format(
                         self.config_file_name
                     )
                 )
@@ -530,7 +545,11 @@ class Writer(BaseSearchClass):
                 )
             cl_mfd.append('--noiseSFTs="{}"'.format(self.noiseSFTs))
         else:
-            cl_mfd.append('--IFOs="{}"'.format(self.detectors))
+            cl_mfd.append(
+                "--IFOs={}".format(
+                    ",".join(['"{}"'.format(d) for d in self.detectors.split(",")])
+                )
+            )
         if self.sqrtSX:
             cl_mfd.append('--sqrtSX="{}"'.format(self.sqrtSX))
 
@@ -559,7 +578,7 @@ class Writer(BaseSearchClass):
         check_ok = self.check_cached_data_okay_to_use(cl_mfd)
         if check_ok is False:
             helper_functions.run_commandline(cl_mfd)
-        logging.info("Successfully wrote SFTs to: {}".format(self.sftfilepath))
+            logging.info("Successfully wrote SFTs to: {}".format(self.sftfilepath))
 
     def predict_fstat(self, assumeSqrtSX=None):
         """ Wrapper to lalapps_PredictFstat """
@@ -916,7 +935,7 @@ transientTau = {:10.0f}\n"""
             )
             logging.info(content)
 
-        if self.check_if_cff_file_needs_rewritting(content):
+        if self.check_if_cff_file_needs_rewriting(content):
             config_file = open(self.config_file_name, "w+")
             config_file.write(content)
             config_file.close()