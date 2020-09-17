fee85c8af681ce2fc2b2e64115fd7188a01d5f39
David Keitel <david.keitel@ligo.org>
2020-09-17 11:28:14 +0200

    further fixup to #178
     -also check np.shape(old_data)[1] < np.shape(new_data)[1]
      to avoid np.allclose() failing

--- pyfstat/grid_based_searches.py
+++ pyfstat/grid_based_searches.py
@@ -234,18 +234,29 @@ class GridSearch(BaseSearchClass):
         # to make np.allclose() work reliably
         new_data = np.nan_to_num(self.input_data.astype(np.float64))
         if np.shape(old_data)[0] != np.shape(new_data)[0]:
-            # only testing number of points, not number of dimensions, here
-            # because output file can have detstat and post-proc quantities
-            # added and hence have different number of dimensions
-            # (this could in principle be cleverly predicted at this point)
             logging.info(
-                "Old data found in '{:s}', input parameters grid differs"
+                "Old data found in '{:s}', but differs"
                 " in length ({:d} points in file, {:d} points requested);"
                 " continuing with grid search.".format(
-                    self.out_file, np.shape(old_data)[0], np.shape(new_data)[1]
+                    self.out_file, np.shape(old_data)[0], np.shape(new_data)[0]
                 )
             )
             return False
+        if np.shape(old_data)[1] < np.shape(new_data)[1]:
+            logging.info(
+                "Old data found in '{:s}', but has less columns ({:d})"
+                " than new input parameters grid ({:d});"
+                " continuing with grid search.".format(
+                    self.out_file, np.shape(old_data)[1], np.shape(new_data)[1]
+                )
+            )
+            return False
+        # not yet explicitly testing the case of
+        # np.shape(old_data)[1] >= np.shape(new_data)[1]
+        # because output file can have detstat and post-proc quantities
+        # added and hence have different number of dimensions
+        # (this could in principle be cleverly predicted at this point)
+        # and the np.allclose() check should safely catch those situations
         rtol, atol = self._get_tolerance_from_savetxt_fmt()
         column_matches = [
             np.allclose(
