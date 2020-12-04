2ba6808cd8ed8f5c9a72d9f9afb7c06386cce89d
Rodrigo-Tenorio <rodrigo.tenorio.marquez@gmail.com>
2020-12-04 11:37:21 +0100

    Add example to produce CW spectrograms

---
+++ examples/other_examples/spectrogram_example.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+
+import pyfstat
+
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["text.usetex"] = True
+
+label = "track_example"
+outdir = os.path.join(sys.path[0], "tmp")
+
+depth = 1
+
+data_parameters = {
+    "sqrtSX": 1e-23,
+    "tstart": 1000000000,
+    "duration": 2 * 365 * 86400,
+    "detectors": "H1",
+    "Tsft": 1800,
+}
+
+signal_parameters = {
+    "F0": 100.0,
+    "F1": 0,
+    "F2": 0,
+    "Alpha": 0.0,
+    "Delta": 0.5,
+    "tp": data_parameters["tstart"],
+    "asini": 25.0,
+    "period": 45 * 86400,
+    "tref": data_parameters["tstart"],
+    "h0": data_parameters["sqrtSX"] / depth,
+    "cosi": 1.0,
+}
+
+data = pyfstat.BinaryModulatedWriter(
+    label=label, outdir=outdir, **data_parameters, **signal_parameters
+)
+data.make_data()
+
+times, freqs, sft_data = pyfstat.helper_functions.get_sft_array(data.sftfilepath)
+
+fig, ax = plt.subplots(figsize=(0.5 * 16, 0.5 * 9))
+ax.grid(which="both")
+ax.set(xlabel="Time [days]", ylabel="Frequency [Hz]", ylim=(99.98, 100.02))
+c = ax.pcolormesh(
+    (times - times[0]) / 86400,
+    freqs,
+    sft_data / data_parameters["sqrtSX"],
+    cmap="inferno_r",
+)
+fig.colorbar(c, label="Normalized Power")
+fig.savefig("spectrogram_example.pdf")
