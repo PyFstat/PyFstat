3bf80c7bc036a25a2485d8fd6893f11c383e2a0d
David Keitel <david.keitel@ligo.org>
2020-12-30 14:27:24 +0100

    sphinx: autoclass_content = 'both'
    - includes documentation of each __init__() method into the documentation of its class

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,3 +59,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- autodoc options ---------------------------------------------------------
+# what content will be inserted into the main body of an autoclass directive
+# both: the class’ and the __init__ method’s docstring are concatenated and inserted.
+autoclass_content = "both"
