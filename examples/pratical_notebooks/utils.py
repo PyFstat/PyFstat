14081d0b9e1683a91f9d0879d28b0e81656d8331
Rodrigo-Tenorio <rodrigo.tenorio@ligo.org>
2022-05-04 16:31:44 +0100

    Finish up tutorial on generating noise

--- examples/pratical_notebooks/utils.py
+++ examples/pratical_notebooks/utils.py
@@ -17,24 +17,51 @@ def plot_real_imag_spectrograms(timestamps, frequency, fourier_data):
     fig, axs = plt.subplots(1, 2, figsize=(16, 10))
 
     for ax in axs:
-        ax.set(xlabel="Days since the start of O3", ylabel="Frequency [Hz]")
+        ax.set(xlabel="SFT index", ylabel="Frequency [Hz]")
 
-    time_in_days = (timestamps - timestamps[0]) / 86400
+    time_in_days = (timestamps - timestamps[0]) / 1800
 
     axs[0].set_title("SFT Real part")
     c = axs[0].pcolormesh(
-        time_in_days, frequency, fourier_data.real, norm=colors.CenteredNorm()
+        time_in_days,
+        frequency,
+        fourier_data.real,
+        norm=colors.CenteredNorm(),
     )
     fig.colorbar(c, ax=axs[0], orientation="horizontal", label="Fourier Amplitude")
 
     axs[1].set_title("SFT Imaginary part")
     c = axs[1].pcolormesh(
-        time_in_days, frequency, fourier_data.imag, norm=colors.CenteredNorm()
+        time_in_days,
+        frequency,
+        fourier_data.imag,
+        norm=colors.CenteredNorm(),
     )
 
     fig.colorbar(c, ax=axs[1], orientation="horizontal", label="Fourier Amplitude")
 
     return fig, axs
+
+
+def plot_real_imag_spectrograms_with_gaps(timestamps, frequency, fourier_data, Tsft):
+
+    # Fill up gaps with Nans
+    gap_length = timestamps[1:] - (timestamps[:-1] + Tsft)
+
+    gap_data = [fourier_data[:, 0]]
+    gap_timestamps = [timestamps[0]]
+
+    for ind, gap in enumerate(gap_length):
+        if gap > 0:
+            gap_data.append(np.full_like(fourier_data[:, ind], np.nan + 1j * np.nan))
+            gap_timestamps.append(timestamps[ind] + Tsft)
+
+        gap_data.append(fourier_data[:, ind + 1])
+        gap_timestamps.append(timestamps[ind + 1])
+
+    return plot_real_imag_spectrograms(
+        np.hstack(gap_timestamps), frequency, np.vstack(gap_data).T
+    )
 
 
 def plot_real_imag_histogram(fourier_data, theoretical_stdev=None):
@@ -73,9 +100,9 @@ def plot_amplitude_phase_spectrograms(timestamps, frequency, fourier_data):
     fig, axs = plt.subplots(1, 2, figsize=(16, 10))
 
     for ax in axs:
-        ax.set(xlabel="Days since the start of O3", ylabel="Frequency [Hz]")
+        ax.set(xlabel="SFT index", ylabel="Frequency [Hz]")
 
-    time_in_days = (timestamps - timestamps[0]) / 86400
+    time_in_days = (timestamps - timestamps[0]) / 1800
 
     axs[0].set_title("SFT absolute value")
     c = axs[0].pcolorfast(
