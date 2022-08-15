7f6d3405f785eaf91b400ac82ef3a9a1c0cef1dc
Rodrigo Tenorio <rodrigo.tenorio@ligo.org>
2022-08-15 16:00:25 +0200

    Reshuffle logic for assumeSqrtSX and noiseWeights

--- pyfstat/snr.py
+++ pyfstat/snr.py
@@ -56,20 +56,19 @@ class SignalToNoiseRatio:
 
     def __attrs_post_init__(self):
 
-        self.Tsft = self.detector_states.data[0].deltaT
+        have_noise_weights = self.noise_weights is not None
+        have_sqrtSX = self.assumeSqrtSX is not None
 
-        # FIXME: There has to be a simpler logic for this
-        if self.noise_weights is None:
-            if self.assumeSqrtSX is not None:
-                self.Sinv_Tsft = self.Tsft / self.assumeSqrtSX**2
-            else:
-                raise ValueError(
-                    "Need either `assumeSqrtSX` or `noise_weights` to account for background noise"
-                )
-        elif self.assumeSqrtSX is not None:
+        if have_noise_weights == have_sqrtSX:
             raise ValueError(
-                "Need either `assumeSqrtSX` or `noise_weights` to account for background noise"
+                "Need either `assumeSqrtSX` or `noise_weights` to account for background noise."
             )
+
+        self.Tsft = self.detector_states.data[0].deltaT
+        if not have_noise_weights:
+            self.Sinv_Tsft = self.Tsft / self.assumeSqrtSX**2
+        else:
+            self.Sinv_Tsft = None
 
     @classmethod
     def from_sfts(
