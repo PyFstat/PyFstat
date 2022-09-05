b4303a492bf18609f7835a1fe7db0ecf862f1738
David Keitel <david.keitel@ligo.org>
2022-09-05 15:28:06 +0200

    split up and improve run_commandline tests

--- tests/test_helper_functions.py
+++ tests/test_helper_functions.py
@@ -17,51 +17,65 @@ def test_gps_to_datestr_utc():
     assert new_str.rstrip(" UTC") == old_str.replace(" GMT ", " ")
 
 
-def test_run_commandline(caplog):
+def test_run_commandline_noreturn():
+    assert run_commandline("echo 'Testing no return'", return_output=False) is None
 
+
+def test_run_commandline_goodreturn(caplog):
+    return_obj = run_commandline("echo 'Testing return'", return_output=True)
+    _, log_level, log_message = caplog.record_tuples[-1]
+    assert type(return_obj) is CompletedProcess
+    assert log_level == 20
+    assert return_obj.stdout == log_message + "\n"
+
+
+def test_run_commandline_stdout_stderr(caplog):
     # Test warning for pipes and proper stdout and stderr redirect
-    msg = "Just a flesh wound"
-    run_commandline(f"echo '{msg}' | tee /dev/stderr")
-
+    msg = "This is a simulated error message, do not worry human!"
+    run_commandline(
+        f"echo '{msg}' | tee /dev/stderr"
+    )  # print this to both stdout and stderr
+    # check that the messages came through at the right logging levels
+    # 20 == INFO, 30 == WARNING, 40 == ERROR
     messages = caplog.record_tuples[-3:]
     assert (messages[0][1] == 30) and (
         "Pipe ('|') found in commandline" in messages[0][2]
     )
-    assert (messages[1][1] == 20) and (messages[1][2] == msg + "\n")
-    assert (messages[2][1] == 40) and (messages[2][2] == msg + "\n")
+    assert messages[1][1] == 20
+    assert messages[1][2] == msg
+    assert messages[2][1] == 40
+    assert messages[2][2] == msg
 
-    # Test proper return without errors
-    return_obj = run_commandline(
-        "echo 'Testing return' | tee /dev/stderr", return_output=True
-    )
-    messages = caplog.record_tuples[-2:]
-    assert type(return_obj) is CompletedProcess
-    assert (messages[0][1] == 20) and (return_obj.stdout == messages[0][2])
-    assert (messages[1][1] == 40) and (return_obj.stderr == messages[1][2])
 
-    assert run_commandline("echo 'Testing no return'", return_output=False) is None
-
-    # Test in case of errors
+def test_run_commandline_expected_error_with_raise(caplog):
     with pytest.raises(CalledProcessError):
-        run_commandline("ls shrubbery 1>&2", raise_error=True)
+        run_commandline("ls nonexistent-directory 1>&2", raise_error=True)
     assert caplog.record_tuples[-1][1] == 40
-    assert run_commandline("ls shrubbery", raise_error=False) is None
 
 
-def test_run_commandline_on_lal(caplog):
+def test_run_commandline_expected_error_noraise(caplog):
+    assert run_commandline("ls nonexistent-directory", raise_error=False) is None
 
+
+def test_run_commandline_lal_noerror(caplog):
     # Basic print should come out as INFO
     return_obj = run_commandline("lalpulsar_version", return_output=True)
-
-    _, log_level, log_message = caplog.record_tuples[-1]
-    assert log_level == 20
-    assert return_obj.stdout == log_message
     assert return_obj.stderr == ""
     assert return_obj.returncode == 0
+    for return_line, (_, log_level, log_message) in zip(
+        return_obj.stdout.split("\n"), caplog.record_tuples[1:]
+    ):
+        assert log_level == 20
+        assert return_line == log_message
 
+
+def test_run_commandline_lal_expected_error(caplog):
     # Errors should go through stderr and come out as ERROR
     with pytest.raises(CalledProcessError):
-        return_obj = run_commandline("lalpulsar_Makefakedata_v5", return_output=True)
+        return_obj = run_commandline(
+            "lalpulsar_version --nonexistent-option-for-simulated-error",
+            return_output=True,
+        )
         _, log_level, log_message = caplog.record_tuples[-1]
         assert log_level == 40
         assert return_obj.stderr == log_message
