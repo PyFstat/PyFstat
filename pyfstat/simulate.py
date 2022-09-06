7d8eb9f469666b6c29ab29b2af255b63c13c3b79
Rodrigo Tenorio <rodrigo.tenorio@ligo.org>
2022-09-06 16:16:25 +0200

    WIP Cleaning up basic interface

--- pyfstat/simulate.py
+++ pyfstat/simulate.py
@@ -1,131 +1,96 @@
-from typing import Union
+import logging
+from typing import Optional, Union
 
 import lal
 import lalpulsar
 import numpy as np
-from attrs import define, field
+from attrs import converters, define, field
 
-import pyfstat.helper_functions as helper_functions
 from pyfstat.snr import DetectorStates, SignalToNoiseRatio
+from pyfstat.utils import get_ephemeris_files
+
+logger = logging.getLogger(__name__)
 
 
 @define(kw_only=True, slots=False)
 class MakeFakeData:
 
     ephemeris: lalpulsar.EphemerisData = field(
-        factory=lambda: lalpulsar.InitBarycenter(
-            *helper_functions.get_ephemeris_files()
-        )
-    )
-    fMin: Union[float, None] = field(default=None)
-    Band: Union[float, None] = field(default=None)
-
-    timestamps: Union[np.array, dict, None] = field()
-    Tsft: Union[int, None] = field(converter=int, default=None)
-    detectors: Union[list, None] = field(
-        default=None, converter=helper_functions.convert_css_to_list
+        factory=lambda: lalpulsar.InitBarycenter(*get_ephemeris_files())
     )
 
-    sqrtSX: Union[list, None] = field(
-        default=None,
-        converter=helper_functions.convert_css_to_list,
-    )
-
-    randSeed: Union[int, None] = field(default=0)
-
-    def __attrs_post_init__(self):
-        self.set_data_parameters()
-
-    def set_data_parameters(self):
-        (
-            self.multiTimestamps,
-            self.multiIFO,
-        ) = DetectorStates.parse_timestamps_and_detectors(
-            self.timestamps, self.Tsft, self.detectors
-        )
-        self.multiNoiseFloor = lalpulsar.MultiNoiseFloor()
-        lalpulsar.ParseMultiNoiseFloor(
-            self.multiNoiseFloor, self.sqrtSX, len(self.detectors)
-        )
-
-        self.data_parameters = lalpulsar.CWMFDataParams()
-        for param in [
-            "multiIFO",
-            "multiNoiseFloor",
-            "multiTimestamps",
-            "randSeed",
-            "fMin",
-            "Band",
-        ]:
-            setattr(self.data_parameters, param, getattr(self, param))
-
-    def set_fMin_Band(self, new_fMin=None, new_Band=None):
-        self.fMin = new_fMin or self.fMin
-        self.Band = new_Band or self.Band
-        if hasattr(self, "data_parameters"):
-            self.data_parameters.fMin = self.fMin
-            self.data_parameters.Band = self.Band
-
-    def parse_signal_parameters(
+    def set_data_params(
         self,
-        refTime,
-        F0,
-        F1,
-        Alpha,
-        Delta,
-        psi,
-        phi0,
-        aPlus=None,
-        aCross=None,
-        h0=None,
-        cosi=None,
+        fMin: float,
+        Band: float,
+        Tsft: int,
+        timestamps: dict,
+        sqrtSX: dict,
+        randSeed: int = 0,
     ):
-        # FIXME: Discuss this input format
 
-        aPlus, aCross = SignalToNoiseRatio._convert_amplitude_parameters(
-            h0=h0, cosi=cosi, aPlus=aPlus, aCross=aCross
+        self.data_params = lalpulsar.CWMFDataParams()
+        self.data_params.randSeed = randSeed
+        self.data_params.fMin = fMin
+        self.data_params.Band = Band
+
+        (
+            self.data_params.multiTimestamps,
+            self.data_params.multiIFO,
+        ) = DetectorStates.parse_timestamps_and_detectors(
+            timestamps,
+            Tsft,
+        )
+        self.data_params.multiNoiseFloor = lalpulsar.MultiNoiseFloor()
+        lalpulsar.ParseMultiNoiseFloor(
+            self.multiNoiseFloor, sqrtSX, self.multiIFO.length
         )
 
-        ppv = lalpulsar.CreatePulsarParamsVector(1)
+    # def set_from_noise_sfts(cls, sftpattern, window_type, window_beta):
+    #    pass
 
-        ppv.data[0].Amp.aPlus = aPlus
-        ppv.data[0].Amp.aCross = aCross
-        ppv.data[0].Amp.psi = psi
-        ppv.data[0].Amp.phi0 = phi0
+    # def set_fMin_Band(self, new_fMin=None, new_Band=None):
+    #    self.fMin = new_fMin or self.fMin
+    #    self.Band = new_Band or self.Band
+    #    if hasattr(self, "data_parameters"):
+    #        self.data_parameters.fMin = self.fMin
+    #        self.data_parameters.Band = self.Band
 
-        ppv.data[0].Doppler.refTime = lal.LIGOTimeGPS(float(refTime))
-        ppv.data[0].Doppler.fkdot[0] = F0
-        ppv.data[0].Doppler.fkdot[1] = F1
-        ppv.data[0].Doppler.Alpha = Alpha
-        ppv.data[0].Doppler.Delta = Delta
+    # def parse_signal_parameters(
+    #    self,
+    #    signal_parameters,
+    # ):
+    #    # FIXME: Discuss this input format
+    #    amplitude_parameters =
+    #    {key: signal_parameters.get(key) for key in ["h0", "cosi", "aPlus", "aCross"]}
+    #    aPlus, aCross = SignalToNoiseRatio._convert_amplitude_parameters(
+    #        h0=h0, cosi=cosi, aPlus=aPlus, aCross=aCross
+    #    )
 
-        return ppv
+    #    ppv = lalpulsar.CreatePulsarParamsVector(1)
 
-    @sqrtSX.validator
-    def _check_sqrtSX_size(self, attribute, value):
+    #    ppv.data[0].Amp.aPlus = aPlus
+    #    ppv.data[0].Amp.aCross = aCross
+    #    ppv.data[0].Amp.psi = psi
+    #    ppv.data[0].Amp.phi0 = phi0
 
-        num_noise_floors = len(value)
-        num_detectors = len(self.detectors)
-        if (num_noise_floors > 1) and (num_noise_floors != num_detectors):
-            raise ValueError(
-                "`sqrtSX` should contain one value for each of the detectors "
-                "or a single value for all of them. "
-                f"Found {num_noise_floors} `sqrtSX` values and {num_detectors} detectors"
-            )
+    #    ppv.data[0].Doppler.refTime = lal.LIGOTimeGPS(float(refTime))
+    #    ppv.data[0].Doppler.fkdot[0] = F0
+    #    ppv.data[0].Doppler.fkdot[1] = F1
+    #    ppv.data[0].Doppler.Alpha = Alpha
+    #    ppv.data[0].Doppler.Delta = Delta
 
-    @classmethod
-    def from_noiseSFTs(cls, sftpattern, window_type, window_beta):
-        pass
+    #    return ppv
 
-    def simulate_data_SFT(self, **params):
-        return lalpulsar.CWMakeFakeMultiData(
-            0,
-            None,
-            self.parse_signal_parameters(**params),
-            self.data_parameters,
-            self.ephemeris,
-        )[0]
+    # def simulate_data_SFT(self, **params):
+    #    return lalpulsar.CWMakeFakeMultiData(
+    #        0,
+    #        None,
+    #        self.parse_signal_parameters(**params),
+    #        self.data_parameters,
+    #        self.ephemeris,
+    #    )[0]
 
-    def simulate_data(self, **params):
-        sfts = self.simulate_data_SFT(**params)
-        return helper_functions.unpack_multi_sft_into_dicts(sfts)
+    # def simulate_data(self, **params):
+    #    sfts = self.simulate_data_SFT(**params)
+    #    return helper_functions.unpack_multi_sft_into_dicts(sfts)
--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -8,59 +8,97 @@ import pyfstat
 
 
 @pytest.fixture
-def mfd_parameters():
+def data_params():
     return {
         "fMin": 10,
         "Band": 1,
         "sqrtSX": "1e-23",
-        "timestamps": 1000000000 + 1800 * np.arange(5),
+        "timestamps": dict.fromkeys(["H1", "L1"], 1000000000 + 1800 * np.arange(5)),
         "Tsft": 1800,
-        "detectors": ["H1", "L1"],
         "randSeed": 314192,
     }
 
 
-@pytest.fixture
-def signal_parameters(mfd_parameters):
-    return {
-        "F0": mfd_parameters["fMin"] + 0.5 * mfd_parameters["Band"],
-        "F1": 0.0,
-        "Alpha": 0,
-        "Delta": 0,
-        "refTime": mfd_parameters["timestamps"][0],
-        "h0": 1e-23,
-        "cosi": 1.0,
-        "psi": 0,
-        "phi0": 0,
-    }
+# @pytest.fixture
+# def signal_parameters(obsrun_parameters):
+#    return {
+#        "F0": mfd_parameters["fMin"] + 0.5 * mfd_parameters["Band"],
+#        "F1": 0.0,
+#        "Alpha": 0,
+#        "Delta": 0,
+#        "refTime": mfd_parameters["timestamps"][0],
+#        "h0": 1e-23,
+#        "cosi": 1.0,
+#        "psi": 0,
+#        "phi0": 0,
+#    }
+#
+# @pytest.fixture
+# def noise_writer(obsrun_parameters, tmp_path):
+#    writer_parameters = {**obsrun_parameters}
+#
+#    for new, old in [("tref", "refTime")]:
+#        writer_parameters[new] = w_parameters.pop(old)
+#    writer_parameters["detectors"] = ",".join(writer_parameters.pop("detectors"))
+#
+#    this_writer = pyfstat.Writer(**writer_parameters, outdir=tmp_path, label="NoiseOnly")
+#    return this_writer
+#
+#
+# @pytest.fixture
+# def signal_writer(obsrun_parameters, signal_parameters, tmp_path):
+#    writer_parameters = {**obsrun_parameters, **signal_parameters}
+#
+#    for remove in ["fMin"]:
+#        writer_parameters.pop(remove)
+#    for new, old in [("tref", "refTime"), ("phi", "phi0")]:
+#        writer_parameters[new] = w_parameters.pop(old)
+#    writer_parameters["detectors"] = ",".join(writer_parameters.pop("detectors"))
+#
+#    this_writer = pyfstat.Writer(**writer_parameters, outdir=tmp_path, label="NoiseAndSignal")
+#    return this_writer
 
 
-@pytest.fixture
-def consistent_writer(mfd_parameters, signal_parameters):
-    w_parameters = {**mfd_parameters, **signal_parameters}
+def test_MakeFakeData_set_data_params(data_params):
 
-    for remove in ["fMin"]:
-        w_parameters.pop(remove)
-    for new, old in [("tref", "refTime"), ("phi", "phi0")]:
-        w_parameters[new] = w_parameters.pop(old)
-    w_parameters["detectors"] = ",".join(w_parameters.pop("detectors"))
-
-    this_writer = pyfstat.Writer(**w_parameters, outdir="MakeFakeDataTest")
-    yield this_writer
-    if os.path.isdir(this_writer.outdir):
-        shutil.rmtree(this_writer.outdir)
+    mfd = pyfstat.MakeFakeData()
+    print(type(data_params["timestamps"]))
+    mfd.set_data_params(**data_params)
+    for key, val in data_params.items():
+        assert getattr(mfd.data_params, key) == value
 
 
-def test_MakeFakeData(consistent_writer, mfd_parameters, signal_parameters):
-    mfd = pyfstat.make_sfts.MakeFakeData(**mfd_parameters)
-    mfd_freq, mfd_timestamps, mfd_amplitudes = mfd.simulate_data(**signal_parameters)
-
-    consistent_writer.make_data()
-    w_freq, w_timestamps, w_amplitudes = pyfstat.helper_functions.get_sft_as_arrays(
-        consistent_writer.sftfilepath
-    )
-
-    np.testing.assert_allclose(mfd_freq, w_freq)
-    for ifo in mfd_parameters["detectors"]:
-        np.testing.assert_allclose(mfd_timestamps[ifo], w_timestamps[ifo])
-        np.testing.assert_allclose(mfd_amplitudes[ifo], w_amplitudes[ifo])
+# def test_MakeFakeData(signal_writer, obsrun_parameters, signal_parameters):
+#    mfd = pyfstat.make_sfts.MakeFakeData()
+#
+#    mfd.set_run_parameters(**obsrun_parameters)
+#    mfd_freq, mfd_timestamps, mfd_amplitudes = mfd.simulate(**signal_parameters)
+#
+#    signal_writer.make_data()
+#    w_freq, w_timestamps, w_amplitudes = pyfstat.utils.get_sft_as_arrays(
+#        signal_writer.sftfilepath
+#    )
+#
+#    np.testing.assert_allclose(mfd_freq, w_freq)
+#    for ifo in mfd_parameters["detectors"]:
+#        np.testing.assert_allclose(mfd_timestamps[ifo], w_timestamps[ifo])
+#        np.testing.assert_allclose(mfd_amplitudes[ifo], w_amplitudes[ifo])
+#
+# def test_MakeFakeData_noiseSFTs(noise_writer, obsrun_parameters, signal_parameters):
+#
+#    noise_writer.make_data()
+#
+#    mfd = pyfstat.make_sfts.MakeFakeData()
+#
+#    mfd.set_from_noise_sfts(noise_writer.sftfilepath, noise_writer.SFTWindowType, noise_writer.SFTWindowBeta)
+#    mfd_freq, mfd_timestamps, mfd_amplitudes = mfd.simulate(**signal_parameters)
+#
+#    writer = pyfstat.Writer(noiseSFTs=noise_writer.sftfilepath, **signal_parameters)
+#    w_freq, w_timestamps, w_amplitudes = pyfstat.utils.get_sft_as_arrays(
+#        consistent_writer.sftfilepath
+#    )
+#
+#    np.testing.assert_allclose(mfd_freq, w_freq)
+#    for ifo in mfd_parameters["detectors"]:
+#        np.testing.assert_allclose(mfd_timestamps[ifo], w_timestamps[ifo])
+#        np.testing.assert_allclose(mfd_amplitudes[ifo], w_amplitudes[ifo])
