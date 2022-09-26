b64e580240a62e4165c9f81536323b29dd07c061
David Keitel <david.keitel@ligo.org>
2022-09-26 19:10:09 +0200

    test_synth: use SNR for h0>0 case

--- tests/test_synth.py
+++ tests/test_synth.py
@@ -14,17 +14,23 @@ def timestamps():
     return np.arange(tstart, tstart + duration, Tsft)
 
 
-def test_synth_CW(timestamps):
+@pytest.mark.parametrize("h0", [0, 1])
+def test_synth_CW(timestamps, h0, detectors="H1"):
+
+    signal_params = {
+        "h0": h0,
+        "cosi": 0,
+        "psi": 0,
+        "phi": 0,
+        "Alpha": 0,
+        "Delta": 0,
+    }
+
     synth = pyfstat.Synthesizer(
         label="Test",
         outdir="TestData/",
-        Alpha=0,
-        Delta=0,
-        h0=0,
-        cosi=0,
-        psi=0,
-        phi=0,
-        detectors="H1",
+        **signal_params,
+        detectors=detectors,
         timestamps=timestamps,
         transientStartTime=0,
         transientTau=timestamps[-1] - timestamps[0],
@@ -32,10 +38,24 @@ def test_synth_CW(timestamps):
         randSeed=0,
     )
 
+    params_pfs = signal_params.copy()
+    params_pfs.pop("phi")
+    detstates = pyfstat.snr.DetectorStates()
+    snr = pyfstat.SignalToNoiseRatio(
+        detector_states=detstates.get_multi_detector_states(
+            timestamps=timestamps,
+            detectors=detectors,
+            Tsft=timestamps[1] - timestamps[0],
+        ),
+        assumeSqrtSX=1,
+    )
+    twoF_from_snr2, twoF_stdev_from_snr2 = snr.compute_twoF(**signal_params)
+    logging.info(f"expected twoF: {twoF_from_snr2}")
+
     twoF = synth.synth_Fstats(numDraws=1)
     logging.info(f"first draw of 2F: {twoF}")
     assert twoF > 0
 
     twoF = synth.synth_Fstats(numDraws=1000)
     logging.info(f"mean over 1000 draws of 2F: {np.mean(twoF)}")
-    assert pytest.approx(np.mean(twoF), rel=0.01) == 4.0
+    assert pytest.approx(np.mean(twoF), rel=0.01) == twoF_from_snr2
