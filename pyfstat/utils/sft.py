a75e11e4805f0c7f6ecb29840f894d7e6874a062
Maria Antonia Ferrer <maria-antonia.ferrer@ligo.org>
2024-10-08 12:00:13 +0200

    Implement function for plotting spectograms. 	Implemented as a function in the sft module.

--- pyfstat/utils/sft.py
+++ pyfstat/utils/sft.py
@@ -1,7 +1,9 @@
 import logging
+import os
 from typing import Dict, Optional, Tuple
 
 import lalpulsar
+import matplotlip as plt
 import numpy as np
 
 logger = logging.getLogger(__name__)
@@ -43,7 +45,6 @@ def get_sft_as_arrays(
         Keys correspond to the official detector names as returned by
         lalpulsar.ListIFOsInCatalog.
     """
-
     constraints = constraints or lalpulsar.SFTConstraints()
     if fMin is None and fMax is None:
         fMin = fMax = -1
@@ -172,6 +173,110 @@ def get_official_sft_filename(
     spec.SFTtimebase = Tsft
     spec.gpsStart = tstart
     # possible gotcha: duration may be different if nanoseconds of sft-epochs are non-zero
-    # (see SFTfileIO.c in lalpulsar)
-    spec.SFTspan = duration
-    return lalpulsar.BuildSFTFilenameFromSpec(spec)
+
+
+def plot_real_imag_spectrograms(
+    self,
+    sftfilepattern: str,
+    outdir: str,
+    label: str,
+    quantity: Optional[str] = "norm_Power",
+    fMin: Optional[float] = None,
+    fMax: Optional[float] = None,
+    constraints: Optional[lalpulsar.SFTConstraints] = None,
+    **kwargs,
+):
+    """
+    Compute spectrograms of a set of SFTs.
+    This is useful to produce visualizations of the Doppler modulation of a CW signal.
+
+    Parameters
+    ----------
+    sftfilepattern:
+        Pattern to match SFTs using wildcards (`*?`) and ranges [0-9];
+        multiple patterns can be given separated by colons.
+    outdir:
+        Output folder.
+    label:
+        Output filename.
+    quantity:
+        Magnitude to be plotted.
+        It can be "norm_Power" for normalized power, "Re" for the real part of
+        the SFTs, and "Im" for the imaginary part of the SFTs.
+    fMin, fMax:
+        Restrict frequency range to `[fMin, fMax]`.
+        If None, retrieve the full frequency range.
+    constraints:
+        Constrains to be fed into XLALSFTdataFind to specify detector,
+        GPS time range or timestamps to be retrieved.
+    kwarg: dict
+        Other kwargs.
+
+    Returns
+    -------
+    ax: matplotlib.axes._subplots_AxesSubplot
+        The axes object containing the plot.
+    """
+
+    outpath = os.path.join(outdir, label)
+    logger = logging.set_up_logger(label=label, outdir=outpath)
+
+    logger.info("Loading SFT data")
+    frequency, timestamps, fourier_data = get_sft_as_arrays(
+        sftfilepattern, fMin, fMax, constraints
+    )
+
+    plotfile = os.path.join(outdir, label + ".png")
+    logger.info(f"Plotting to file: {plotfile}")
+
+    plt.rcParams["axes.grid"] = False  # turn off the gridlines
+    fig, ax = plt.subplots(figsize=(0.8 * 16, 0.8 * 9))
+    ax.set(xlabel="Time [days]", ylabel="Frequency [Hz]")
+
+    time_in_days = (timestamps - timestamps[0]) / 86400
+
+    if quantity == "norm_Power":
+        logger.info("Computing normalized power")
+        sft_power = fourier_data.real**2 + fourier_data.imag**2
+        normalized_power = sft_power
+        # (2 * sft_power / (data_parameters["Tsft"] * data_parameters["sqrtSX"] ** 2)
+        c = ax.pcolormesh(
+            time_in_days,
+            frequency,
+            normalized_power,
+            cmap="inferno_r",
+            shading="nearest",
+        )
+        fig.colorbar(c, label="Normalized Power")
+
+    elif quantity == "Re":
+        c = ax.pcolormesh(
+            time_in_days,
+            frequency,
+            fourier_data.real,
+            cmap="inferno_r",
+            shading="nearest",
+        )
+        ax.set_title("SFT Real part")
+        fig.colorbar(c, label="Fourier amplitude")
+
+    elif quantity == "Im":
+        c = ax.pcolormesh(
+            time_in_days,
+            frequency,
+            fourier_data.imag,
+            cmap="inferno_r",
+            shading="nearest",
+        )
+        ax.set_title("SFT Imaginary part")
+        fig.colorbar(c, label="Fourier amplitude")
+
+    else:
+        raise ValueError(
+            "String `quantity` not accepted. Please, introduce a valid string"
+        )
+
+    plt.tight_layout()
+    fig.savefig(plotfile)
+
+    return ax
