245b93d6f163638a1c592c8df0ee58f9a3105c8f
David Keitel <david.keitel@ligo.org>
2024-10-28 18:53:54 +0100

    test_sft.py: improve fixture/parametrize usage
     - @pytest.fixture(scope="module") avoids data regenreation
     - @pytest.fixture(params=[1,2]) will be inherited by all functions using it
     - also simplify the actual timestamps setup

--- tests/test_utils/test_sft.py
+++ tests/test_utils/test_sft.py
@@ -8,118 +8,78 @@ from pyfstat import Writer
 from pyfstat.utils import get_sft_as_arrays, plot_spectrogram
 
 
-@pytest.fixture
-def timestamps_for_test():
-    # Set up SFTs with arbitrary gaps
-    return {
-        "H1": 1800 * np.array([1, 2, 3, 4, 5, 6]),
-        "L1": 1800 * np.array([1, 2, 3, 4, 5, 6]),
+@pytest.fixture(scope="module", params=["nogaps", "gaps"])
+def data_for_test(tmp_path_factory, request):
+    Tsft = 1800
+    ts_base = np.arange(start=1, stop=7 * Tsft, step=Tsft, dtype=int)
+    timestamps = {
+        "H1": ts_base,
+        "L1": ts_base,
     }
-
-
-@pytest.fixture
-def timestamps_for_test_with_gaps():
-    # Set up SFTs with arbitrary gaps
-    return {
-        "H1": np.array(
-            [
-                1000000000,
-                1000000000 + 1800,
-                1000000000 + 3600,
-                1000000000 + 10000,
-                1000000000 + 11800,
-            ]
-        ),
-        "L1": 1238166018 + 1800 * np.array([0, 2, 4, 8]),
-    }
-
-
-@pytest.fixture
-def data_for_test(tmp_path, timestamps_for_test):
+    gaps = request.param
+    if gaps == "gaps":
+        for ifo in timestamps.keys():
+            timestamps[ifo] = np.delete(timestamps[ifo], [2, 4])
     writer_kwargs = {
         "sqrtSX": 1,
         "Tsft": 1800,
-        "timestamps": timestamps_for_test,
+        "timestamps": timestamps,
         "detectors": "H1,L1",
         "SFTWindowType": "tukey",
         "SFTWindowParam": 0.001,
         "randSeed": 42,
         "F0": 10.0,
         "Band": 0.1,
-        "label": "TestingSftToArray",
-        "outdir": tmp_path,
+        "label": "TestSFTs" + gaps,
+        "outdir": tmp_path_factory.mktemp("SFTdata"),
     }
     writer = Writer(**writer_kwargs)
     writer.make_data()
-
-    return writer
-
-
-@pytest.fixture
-def data_for_test_with_gaps(tmp_path, timestamps_for_test_with_gaps):
-    writer_kwargs = {
-        "sqrtSX": 1,
-        "Tsft": 1800,
-        "timestamps": timestamps_for_test_with_gaps,
-        "detectors": "H1,L1",
-        "SFTWindowType": "tukey",
-        "SFTWindowParam": 0.001,
-        "randSeed": 42,
-        "F0": 10.0,
-        "Band": 0.1,
-        "label": "TestingSftToArray",
-        "outdir": tmp_path,
-    }
-    writer = Writer(**writer_kwargs)
-    writer.make_data()
-
-    return writer
+    return writer, timestamps
 
 
-def test_get_sft_as_arrays(data_for_test, timestamps_for_test):
+def test_get_sft_as_arrays(data_for_test):
 
-    frequency_step = 1 / data_for_test.Tsft
-    num_frequencies = np.floor(data_for_test.Band * data_for_test.Tsft + 0.5)
+    writer, timestamps = data_for_test
+
+    frequency_step = 1 / writer.Tsft
+    num_frequencies = np.floor(writer.Band * writer.Tsft + 0.5)
     expected_frequencies = (
-        data_for_test.F0
-        - data_for_test.Band / 2
-        + frequency_step * np.arange(num_frequencies)
+        writer.F0 - writer.Band / 2 + frequency_step * np.arange(num_frequencies)
     )
 
     # Single detector
-    single_detector_path = data_for_test.sftfilepath.split(";")[0]
-    ifo = data_for_test.detectors.split(",")[0]
+    single_detector_path = writer.sftfilepath.split(";")[0]
+    ifo = writer.detectors.split(",")[0]
     frequencies, times, amplitudes = get_sft_as_arrays(single_detector_path)
     np.testing.assert_allclose(frequencies, expected_frequencies)
-    np.testing.assert_equal(times[ifo], timestamps_for_test[ifo])
+    np.testing.assert_equal(times[ifo], timestamps[ifo])
     assert frequencies.shape + times[ifo].shape == amplitudes[ifo].shape
 
     # Multi-detector
     frequencies, times, amplitudes = get_sft_as_arrays(single_detector_path)
     np.testing.assert_allclose(frequencies, expected_frequencies)
     for ifo in times:
-        np.testing.assert_equal(times[ifo], timestamps_for_test[ifo])
+        np.testing.assert_equal(times[ifo], timestamps[ifo])
         assert frequencies.shape + times[ifo].shape == amplitudes[ifo].shape
 
 
 @pytest.mark.parametrize("quantity", ["power", "normpower", "real", "imag"])
-@pytest.mark.parametrize("data_function", [data_for_test, data_for_test_with_gaps])
-def test_spectrogram(data_function, quantity):
+def test_spectrogram(data_for_test, quantity):
 
-    data = data_function()
+    writer, timestamps = data_for_test
 
+    plotlabel = f"{writer.label}_{quantity}"
     ax = plot_spectrogram(
-        sftfilepattern=data.sftfilepath,
+        sftfilepattern=writer.sftfilepath,
         quantity=quantity,
-        sqrtSX=data.sqrtSX,
-        detector=data.detectors.split(",")[0],
+        sqrtSX=writer.sqrtSX,
+        detector=writer.detectors.split(",")[0],
         savefig=True,
-        outdir=data.outdir,
-        label=data.label,
+        outdir=writer.outdir,
+        label=plotlabel,
     )
-
     assert isinstance(ax, matplotlib.axes.Axes)
 
-    plotfile = os.path.join(data_for_test.outdir, data_for_test.label + ".png")
-
+    plotfile = os.path.join(writer.outdir, plotlabel + ".png")
     assert os.path.isfile(plotfile)
